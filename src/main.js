30f1077aeb3b350f0cc11195b056675dcc6d6b7d
Forge (Hirefel OS) <forge@hirefel.com>
2026-04-05 04:15:27 +0000

    Initial commit: Hello Robot Vite app

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div class="container">
+    <div class="robot">🤖</div>
+    <h1>Hello I'm a robot!</h1>
+    <p class="subtitle">Powered by Hirefel</p>
+  </div>
+`
